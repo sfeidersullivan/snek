627a485925a71e029748e1ae1626fe9aba2fe18b
Calvin Furano <cfurano@solarcity.com>
2016-06-30 11:13:52 -0600

    First

--- bin/runner.js
+++ bin/runner.js
@@ -5,9 +5,8 @@ var program = require('commander');
 var blessed = require('blessed');
 var fs = require('fs');
 var path = require('path');
-var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
-var Ticker = require(lib + '/ticker.js');
-var App = require(lib + '/app.js');
+var Ticker = require('../lib/ticker.js');
+var App = require('../lib/app.js');
 
 program._name = 'blessed-life';
 program.usage('[options] <config>')
@@ -47,15 +46,25 @@ var config = {
 
 var screen = blessed.screen();
 
-config.width = config.width || screen.width;
-config.height = config.height || screen.height;
+config.width = 50;
+config.height = 25;
 
-var app = new App(config);
+var controller = {
+	nextMove: "",
+	
+	getNextMove: function() {
+		var move = this.nextMove;
+		this.nextMove = "";
+		return move;
+	}
+};
+
+var app = new App(config, controller);
 
 // Create ui components
 var box = blessed.box({
-  top: '0',
-  left: '0',
+  top: 3,
+  left: 3,
   width: '100%',
   height: '100%',
   content: app.renderBoard(),
@@ -67,24 +76,6 @@ var box = blessed.box({
 });
 screen.append(box);
 
-var spawnForm = blessed.form({
-	left: 0,
-	bottom: 0,
-	width: '100%',
-	height: 1,
-	content: "Toggle Position? ",
-	hidden: true
-});
-screen.append(spawnForm);
-
-var spawnInput = blessed.textbox({
-	left: spawnForm.content.length,
-	bottom: 0,
-	width: screen.width - spawnForm.content.length,
-	height: 1,
-	hidden: true
-});
-spawnForm.append(spawnInput);
 // finished creating ui components
 
 function onTick() {
@@ -108,34 +99,12 @@ screen.key(['p'], function(ch, key) {
 	ticker.toggle();
 });
 
-screen.key(['i'], function(ch, key) {
-	if (ticker.running) {
-		ticker.stop();
-	}
+screen.key(['a'], function(ch, key) {
+	controller.nextMove = "left";
+});
 
-	spawnForm.show();
-	spawnInput.show();
-	spawnInput.readInput(function onDone(err, value) {
-		spawnForm.hide();
-		spawnInput.hide();
-		spawnInput.setValue('');
-
-		if (value !== null) {
-			value = value.split(/,|;/);
-			for (var i = 0; i < value.length / 2; i++) {
-				var x = parseInt(value[i * 2]);
-				var y = parseInt(value[(i * 2) + 1]);
-
-				if (app.game.board.isInBounds(x, y)) {
-					app.game.board.toggleCell(x, y);
-
-				}
-			}
-			box.setContent(app.renderBoard());
-		}
-		screen.render();
-	});
-	screen.render();
+screen.key(['s'], function(ch, key) {
+	controller.nextMove = "right";
 });
 
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {

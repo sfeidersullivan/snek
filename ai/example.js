51f3e758a31fe2634a40cf00dfde09cf946fce76
Calvin Furano <cfurano@solarcity.com>
2016-10-25 17:36:43 -0600

    Making updates for round 2

--- ai/example.js
+++ ai/example.js
@@ -2,36 +2,42 @@ var _ = require("lodash");
 var elements = require("../lib/elements");
 var directions = require("../lib/directions");
 
+// Give your bot a name!
 module.exports.name = "Naïve Norman";
 
-module.exports.getNextMove = function(game, player) { 
+module.exports.getNextMove = function(game, player, opponent) { 
   /***************************
    * BEGIN EXAMPLE ALGORITHM *
    ***************************/
   var apple = game.board.apple;
   var head = player.snake.getHead();
-  
+
+  var decision;
   switch (head.direction) {
     case directions.NORTH:
-      if (apple.x > head.x) return "right";
-      if (apple.x < head.x) return "left"; 
+      if (apple.x > head.x) decision = "right";
+      else if (apple.x < head.x) decision = "left"; 
       break;
     case directions.SOUTH:
-      if (apple.x < head.x) return "right";
-      if (apple.x > head.x) return "left"; 
+      if (apple.x < head.x) decision = "right";
+      else if (apple.x > head.x) decision = "left"; 
       break;
     case directions.EAST:
-      if (apple.y > head.y) return "right"; 
-      if (apple.y < head.y) return "left";
+      if (apple.y > head.y) decision = "right"; 
+      else if (apple.y < head.y) decision = "left";
       break;
     case directions.WEST:
-      if (apple.y < head.y) return "right"; 
-      if (apple.y > head.y) return "left"; 
+      if (apple.y < head.y) decision = "right"; 
+      else if (apple.y > head.y) decision = "left"; 
       break;
   }
+  
+  if (decision) {
+    console.log("My decision is: %s", decision);    
+  }
+
+  return decision;
   /***************************
    * END EXAMPLE ALGORITHM *
    ***************************/
-  
-  return "";  
 };
